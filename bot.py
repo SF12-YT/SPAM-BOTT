f0f18a8e90813b25f48fc35b0f89b2e2639c7c75
SF12-YT <62644015+SF12-YT@users.noreply.github.com>
2020-04-13 19:02:14 +1000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+﻿import discord
+
+TOKEN = 'NjkxOTg3OTUwNDA1MTU2ODg3.Xnw4yw.g9zF6eFOu-B0BfS45RGtBNw3_Os'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    while True:
+        await message.channel.send("YOU SUC")
+
+client.run(TOKEN)
